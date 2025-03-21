b3e464953690f847856002f8ed5f5ed7bc507e61
luhaoyu <haoyu3@163.com>
2025-03-21 12:55:29 +0800

    add hex 文件

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { registerAESTool } from "./service/aes.js";
 import { registerDigestTool } from "./service/digest.js";
 import { registerDESTool } from "./service/des.js";
 import { registerBase64Tool } from "./service/base64.js";
+import { registerHexTool } from "./service/hex.js";
 // Create an MCP server
 const server = new McpServer({
   name: "crypto-mcp",
@@ -15,6 +16,7 @@ registerAESTool(server);
 registerDigestTool(server);
 registerDESTool(server);
 registerBase64Tool(server);
+registerHexTool(server);
 /*
  * Start the server using stdio transport.
  */
